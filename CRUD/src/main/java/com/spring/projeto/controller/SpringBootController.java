2617958555326d1f9c41ae4a6afeed0e1bb6d1b9
alyson21 <51250873+alyson21@users.noreply.github.com>
2019-09-11 22:18:43 -0300

    Add files via upload

---
+++ CRUD/src/main/java/com/spring/projeto/controller/SpringBootController.java
@@ -0,0 +1,37 @@
+package com.spring.projeto.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootController {
+	
+	private List<String> alunos;
+	
+	public SpringBootController() {
+		alunos = new ArrayList<>();
+	}
+	
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public List<String> get() {
+		return alunos;
+	}
+	
+	@RequestMapping(value = "/teste/load", method = RequestMethod.GET)
+	public void loadList(){
+		alunos.add("Jose");
+		alunos.add("Maria");
+		alunos.add("Joao");
+	}
+	
+	
+	@RequestMapping(value = "/teste/post", method = RequestMethod.POST)
+	public void postAluno(String aluno){
+		alunos.add(aluno);
+	}
+
+}
