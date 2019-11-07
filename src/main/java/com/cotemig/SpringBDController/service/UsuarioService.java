0bd42ed7ab5ecf4cfda346a537a6c23fcc1a74da
usuario <usuario@REDES09.lan>
2019-11-06 22:13:00 -0200

    Implementando services

--- src/main/java/com/cotemig/SpringBDController/service/UsuarioService.java
+++ src/main/java/com/cotemig/SpringBDController/service/UsuarioService.java
@@ -1,5 +1,18 @@
 package com.cotemig.SpringBDController.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.cotemig.SpringBDController.model.Usuario;
+
 public interface UsuarioService {
+	
+	Optional<Usuario> getById(Integer id);
+	List<Usuario> getAll();
+	void deleteAll();
+	void deleteById(Integer id);
+	void updateById(Integer id, Usuario usuario);
+	void update(Usuario usuario);
+	void insert(Usuario usuario);
 
 }
--- src/main/java/com/cotemig/SpringBDController/service/ValoresVariaveisService.java
+++ src/main/java/com/cotemig/SpringBDController/service/ValoresVariaveisService.java
@@ -1,5 +1,18 @@
 package com.cotemig.SpringBDController.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.cotemig.SpringBDController.model.ValoresVariaveis;
+
 public interface ValoresVariaveisService {
+	
+	Optional<ValoresVariaveis> getById(Integer id);
+	List<ValoresVariaveis> getAll();
+	void deleteAll();
+	void deleteById(Integer id);
+	void updateById(Integer id, ValoresVariaveis valoresVariaveis);
+	void update(ValoresVariaveis valoresVariaveis);
+	void insert(ValoresVariaveis valoresVariaveis);
 
 }
