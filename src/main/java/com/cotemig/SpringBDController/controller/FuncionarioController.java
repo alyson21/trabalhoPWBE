f93afdeb4db6262927ed64985ba62fb0b692b8e4
usuario <usuario@REDES08.lan>
2019-11-13 23:28:48 -0200

    alt4erações

--- src/main/java/com/cotemig/SpringBDController/controller/FuncionarioController.java
+++ src/main/java/com/cotemig/SpringBDController/controller/FuncionarioController.java
@@ -3,34 +3,37 @@ package com.cotemig.SpringBDController.controller;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.method.annotation.ModelAndViewResolverMethodReturnValueHandler;
 
 import com.cotemig.SpringBDController.model.Funcionario;
 import com.cotemig.SpringBDController.service.FuncionarioService;
+import com.cotemig.SpringBDController.service.SalarioService;
 
 
-
+@Controller
 public class FuncionarioController {
 	
 	@Autowired
 	private FuncionarioService funcionarioService;
+	@Autowired
+	private SalarioService salarioService;
 	
 	@RequestMapping(value = "/funcionario", method = RequestMethod.GET)
     public ModelAndView listar() {
-		ModelAndView mav = new ModelAndView();
-			return new ModelAndView("funcionario", "Funcionarios", funcionarioService.getAll());
+		
+		return new ModelAndView("funcionario", "Funcionario", funcionarioService.getAll());
 			
     }
 	
 	@RequestMapping(value = "/insertfuncionario", method = RequestMethod.GET)
-    public String insert() {
-			return "insertFuncionario";		
+    public ModelAndView insert() {
+		return new ModelAndView("insertfuncionario", "Salario", salarioService.getAll());	
     }
 	
 	
